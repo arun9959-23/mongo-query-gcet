7ff1dc6af7b43f5e1809c5f4f5bdc79a19d04861
arun9959-23 <23r11a6710@gcet.edu.in>
2025-05-29 18:49:24 +0530

    push

--- q4.js
+++ q4.js
@@ -2,10 +2,12 @@ db.Products.insertMany([
   {
     name: "phone",
     price:20000,
+    imageurl:"https://momunicipallaw.files.wordpress.com/2014/08/cell-phone.png",
   },
   {
     name: "laptop",
     price:100000,
+    imageurl:"https://tse3.mm.bing.net/th?id=OIP.WCCq2nZelTZuFIRbJF7AuAHaEK&pid=Api&P=0&h=180",
   },
 ]);
 
