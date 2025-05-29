b14ce7f1f963c2404bdf6442f9e5c2c945c91f48
arun9959-23 <23r11a6710@gcet.edu.in>
2025-05-29 19:52:03 +0530

    update

---
+++ q6.js
@@ -0,0 +1,18 @@
+db.employees.updateOne(
+  { email: "john@gmail.com" },
+  { $set: { email: "john@outlook.com" } }
+); ////1
+db.employees.find().sort({ salary: -1 }).limit(3);//2
+
+db.employees.updateMany({}, { $inc: { salary: 1000 } });//3
+
+db.employees.insertOne({
+  name: "New Admin",
+  email: "admin@example.com",
+  department: "admin",
+  salary: 3000,
+  location: ["TX"],
+  date: new Date()
+});//4
+
+db.employees.deleteMany({ department: "admin" });//5
